0cff263533006f83c9e7264877eb68374fcdb8d2
IvanIhnatsiuk <ivan.ihnatsiuk@bankifi.com>
2024-10-16 16:50:32 +0200

    fix: apply swiftformat

--- ios/NotifyingMaskedTextFieldListener.swift
+++ ios/NotifyingMaskedTextFieldListener.swift
@@ -10,15 +10,15 @@ import Foundation
 import UIKit
 
 class NotifyingMaskedTextFieldListener: MaskedTextInputListener {
-    override func textField(
-        _ textField: UITextField,
-        shouldChangeCharactersIn range: NSRange,
-        replacementString string: String
-    ) -> Bool {
-        defer {
-            NotificationCenter.default.post(name: UITextField.textDidChangeNotification, object: textField)
-            textField.sendActions(for: .editingChanged)
-        }
-        return super.textField(textField, shouldChangeCharactersIn: range, replacementString: string)
+  override func textField(
+    _ textField: UITextField,
+    shouldChangeCharactersIn range: NSRange,
+    replacementString string: String
+  ) -> Bool {
+    defer {
+      NotificationCenter.default.post(name: UITextField.textDidChangeNotification, object: textField)
+      textField.sendActions(for: .editingChanged)
     }
+    return super.textField(textField, shouldChangeCharactersIn: range, replacementString: string)
+  }
 }
