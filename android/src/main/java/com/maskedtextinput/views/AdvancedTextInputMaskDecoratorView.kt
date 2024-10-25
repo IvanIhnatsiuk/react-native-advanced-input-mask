5c5ba050887c09ee2b0a99d62c923d0634859ee4
IvanIhnatsiuk <ivan.ihnatsiuk@bankifi.com>
2024-10-25 20:36:33 +0200

    fix: apply ktlint

--- android/src/main/java/com/maskedtextinput/views/AdvancedTextInputMaskDecoratorView.kt
+++ android/src/main/java/com/maskedtextinput/views/AdvancedTextInputMaskDecoratorView.kt
@@ -1,7 +1,6 @@
 package com.maskedtextinput.views
 
 import android.content.Context
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import com.facebook.react.bridge.ReactContext
