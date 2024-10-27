fc98a9719655a363a467f411323dc883ee46137d
IvanIhnatsiuk <ivan.ihnatsiuk@bankifi.com>
2024-10-28 00:07:02 +0100

    feat: add jest mocks

---
+++ jest/index.js
@@ -0,0 +1,15 @@
+import { TextInput } from 'react-native';
+
+export const AFFINITY_CALCULATION_STRATEGY = {
+  WHOLE_STRING: 0,
+  PREFIX: 1,
+  CAPACITY: 2,
+  EXTRACTED_VALUE_CAPACITY: 3,
+};
+
+const mock = {
+  default: TextInput,
+  AFFINITY_CALCULATION_STRATEGY,
+};
+
+module.exports = mock;
