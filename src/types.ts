485e911801b4337a3b5f6fbdf8e190d60140d5bf
IvanIhnatsiuk <ivan.ihnatsiuk@bankifi.com>
2024-10-28 00:39:25 +0100

    feat: add-ts-doc

--- src/types.ts
+++ src/types.ts
@@ -16,24 +16,73 @@ export type Notation = {
   isOptional: boolean;
 };
 
+type CustomTransformation = {
+  transformationChar: string;
+  transformationString: string;
+};
+
 export type MaskedTextInputDecoratorViewNativeProps = ViewProps & {
+  /**
+   * The mask format to be applied to the text input.
+   * @example "[0000] [0000] [0000] [0000]"
+   */
   mask: string;
+  /**
+   * Custom notations to be used in the mask format.
+   * Each notation should be an object containing:
+   * - character: The character to be replaced in the mask.
+   * - characterSet: The set of characters that can replace the notation character.
+   * - isOptional: Whether the notation character is optional.
+   */
   customNotations?: Notation[];
+  /**
+   * Callback function to be called when the text changes.
+   * @param formattedValue The formatted value.
+   * @param extractedValue The extracted value.
+   */
   onChangeText?: (formattedValue: string, extractedValue: string) => void;
+  /**
+   * Callback function to be called when tail placeholder changes.
+   * @param tailPlaceholder The tail placeholder.
+   */
   onTailPlaceholderChange?: (tailPlaceholder: string) => void;
+  /**
+   * An array of strings representing the affinity format.
+   * Used to determine the best mask format based on the input.
+   */
   affinityFormat?: string[];
+  /**
+   * Whether to enable autocomplete for the text input.
+   * Default is false.
+   */
   autocomplete?: boolean;
+  /**
+   * Whether to automatically skip to the next input field when the current one is filled.
+   * @default false.
+   */
   autoSkip?: boolean;
+  /**
+   * Whether the text input should support right-to-left (RTL) text direction.
+   * @default false.
+   */
   isRTL?: boolean;
+  /**
+   * The strategy to use for affinity calculation.
+   * Determines how the best mask format is selected based on the input.
+   */
   affinityCalculationStrategy?: AFFINITY_CALCULATION_STRATEGY;
-  secureTextEntry?: boolean;
-  customTransformation?: {
-    transformationChar: string;
-    transformationString: string;
-  };
+  /**
+   * Custom transformation to be applied to the text input.
+   * Defines how the input text should be transformed.
+   */
+  customTransformation?: CustomTransformation;
   defaultValue?: string;
   value?: string;
   // IOS only props
+  /**
+   * Whether to allow suggestions for the text input on iOS.
+   * @default false.
+   */
   allowSuggestions?: boolean;
   autocompleteOnFocus?: boolean;
 };
