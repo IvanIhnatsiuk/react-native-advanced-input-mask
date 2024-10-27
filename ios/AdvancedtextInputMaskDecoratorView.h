4436b3df9c5687566b13b2369815809765f37dda
IvanIhnatsiuk <ivan.ihnatsiuk@bankifi.com>
2024-10-27 15:30:16 +0100

    feat: clang format

--- ios/AdvancedtextInputMaskDecoratorView.h
+++ ios/AdvancedtextInputMaskDecoratorView.h
@@ -11,25 +11,29 @@
 #ifdef RCT_NEW_ARCH_ENABLED
 #import <React/RCTViewComponentView.h>
 #import <UIKit/UIKit.h>
-#import "AdvancedTextInputViewContainer.h"
 #import <react/renderer/components/maskedtextinput/Props.h>
+#import "AdvancedTextInputViewContainer.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface AdvancedTextInputMaskDecoratorView : RCTViewComponentView<AdvancedTextInputViewContainerDelegate>
+@interface AdvancedTextInputMaskDecoratorView
+    : RCTViewComponentView <AdvancedTextInputViewContainerDelegate>
 @end
 
-
 namespace facebook {
-    namespace react {
-        // In order to compare these structs we need to add the == operator for each
-        // TODO: https://github.com/reactwg/react-native-new-architecture/discussions/91#discussioncomment-4426469
-        bool operator==(const AdvancedTextInputMaskDecoratorViewCustomNotationsStruct& a, const AdvancedTextInputMaskDecoratorViewCustomNotationsStruct& b)
-        {
-            return b.character == a.character && b.characterSet == a.characterSet && b.isOptional == a.isOptional;
-        }
-    }
+namespace react {
+// In order to compare these structs we need to add the == operator for each
+// TODO:
+// https://github.com/reactwg/react-native-new-architecture/discussions/91#discussioncomment-4426469
+bool operator==(
+    const AdvancedTextInputMaskDecoratorViewCustomNotationsStruct &a,
+    const AdvancedTextInputMaskDecoratorViewCustomNotationsStruct &b)
+{
+  return b.character == a.character && b.characterSet == a.characterSet &&
+      b.isOptional == a.isOptional;
 }
+} // namespace react
+} // namespace facebook
 
 NS_ASSUME_NONNULL_END
 
--- ios/RNConversions.h
+++ ios/RNConversions.h
@@ -8,30 +8,34 @@
 #ifdef __cplusplus
 #import <React/RCTConversions.h>
 
-inline NSArray<NSDictionary *> *convertCustomNotations(std::vector<facebook::react::AdvancedTextInputMaskDecoratorViewCustomNotationsStruct> customNotationsArray)
+inline NSArray<NSDictionary *> *convertCustomNotations(
+    std::vector<facebook::react::AdvancedTextInputMaskDecoratorViewCustomNotationsStruct>
+        customNotationsArray)
 {
-    NSMutableArray *customNotations = [NSMutableArray arrayWithCapacity: customNotationsArray.size()];
-    
-    for (auto customNotation : customNotationsArray) {
-        NSDictionary *notation = @{
-            @"character": RCTNSStringFromString(customNotation.character),
-            @"characterSet": RCTNSStringFromString(customNotation.characterSet),
-            @"isOptional": @(customNotation.isOptional),
-        };
-        [customNotations addObject: notation];
-    }
-    
-    return customNotations;
+  NSMutableArray *customNotations = [NSMutableArray arrayWithCapacity:customNotationsArray.size()];
+
+  for (auto customNotation : customNotationsArray) {
+    NSDictionary *notation = @{
+      @"character" : RCTNSStringFromString(customNotation.character),
+      @"characterSet" : RCTNSStringFromString(customNotation.characterSet),
+      @"isOptional" : @(customNotation.isOptional),
+    };
+    [customNotations addObject:notation];
+  }
+
+  return customNotations;
 }
 
-inline NSArray<NSString *>* convertAffinityFormat(std::vector<std::string> affinityFormatArray) {
-    NSMutableArray<NSString *> *affinityFormats = [NSMutableArray arrayWithCapacity: affinityFormatArray.size()];
-    
-    for(auto &affinityFormat : affinityFormatArray){
-        [affinityFormats addObject: RCTNSStringFromString(affinityFormat)];
-    }
-    
-    return affinityFormats;
+inline NSArray<NSString *> *convertAffinityFormat(std::vector<std::string> affinityFormatArray)
+{
+  NSMutableArray<NSString *> *affinityFormats =
+      [NSMutableArray arrayWithCapacity:affinityFormatArray.size()];
+
+  for (auto &affinityFormat : affinityFormatArray) {
+    [affinityFormats addObject:RCTNSStringFromString(affinityFormat)];
+  }
+
+  return affinityFormats;
 }
 
 #endif
