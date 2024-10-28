d6a55a2933723cf4f24fc53ed5aa0646388b7dec
IvanIhnatsiuk <86000012+IvanIhnatsiuk@users.noreply.github.com>
2024-10-28 20:15:31 +0100

    chore: react-native 0.76 (#14)
    * feat: react-native-0.76
    
    * fix: generic type
    
    * fix: lint
    
    * fix: iOS build
    
    * fix: run yarn dedupe command
    
    * fix: apply ktlint
    
    * fix: uncomment ios cache logic

--- android/src/newarch/java/com/maskedtextinput/AdvancedTextInputMaskDecoratorViewManagerSpec.kt
+++ android/src/newarch/java/com/maskedtextinput/AdvancedTextInputMaskDecoratorViewManagerSpec.kt
@@ -6,7 +6,7 @@ import com.facebook.react.uimanager.ViewManagerDelegate
 import com.facebook.react.viewmanagers.AdvancedTextInputMaskDecoratorViewManagerDelegate
 import com.facebook.react.viewmanagers.AdvancedTextInputMaskDecoratorViewManagerInterface
 
-abstract class AdvancedTextInputMaskDecoratorViewManagerSpec<T : View?> :
+abstract class AdvancedTextInputMaskDecoratorViewManagerSpec<T : View> :
   SimpleViewManager<T>(),
   AdvancedTextInputMaskDecoratorViewManagerInterface<T> {
   private val mDelegate: ViewManagerDelegate<T> = AdvancedTextInputMaskDecoratorViewManagerDelegate(this)
