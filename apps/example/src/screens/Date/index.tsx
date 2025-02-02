e02ecf450ccc764124158f25aa71f5f89c0f530a
IHNATSIUK, IVAN <ii3234@exo.att.com>
2025-02-02 11:07:11 +0100

    fix: move default value to useMemo

--- apps/example/src/screens/Date/index.tsx
+++ apps/example/src/screens/Date/index.tsx
@@ -7,13 +7,15 @@ import { MaskedTextInput } from 'react-native-advanced-input-mask';
 const affineFormats = ['[00]{/}[00]{/}[00]'];
 
 const DateScreen = () => {
+  const defaultValue = React.useMemo(() => new Date().toISOString(), []);
+
   return (
     <ScrollView
       style={styles.container}
       contentContainerStyle={styles.contentContainer}
     >
       <MaskedTextInput
-        defaultValue={new Date().toISOString()}
+        defaultValue={defaultValue}
         style={styles.maskedTextInput}
         mask="[0000]{/}[00]{/}[00]"
         affinityFormat={affineFormats}
